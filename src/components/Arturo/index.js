2aa4873eaaf7d67e9a2e9584a2b92a69d4df5623
Brian Durand <briandurand1990@gmail.com>
2020-08-05 19:07:03 -0300

    [Fix] ShowContent:
    - El problema era que al no tener tareas almacenadas en localStorage, se enviaba un null por parametro a ShowContent y este no era iterable para renderear en pantalla.
    
    Lo solucione comprobando primero que el primer elemento de la lista de tareas no este sin definir y de estar definido, comprobar si es el mismo mensaje inicial (cuando no hay tareas) y ademas, que haya otro elemento en la lista.
    
    De superar las comprobaciones, se filtra de la lista el indice que posea el mensaje inicial y se deja la tarea nueva.
    
    Estuvo divertido.. se aprendio bastante.

--- src/components/Arturo/index.js
+++ src/components/Arturo/index.js
@@ -5,12 +5,21 @@ import ShowContent from "../ShowContent";
 
 const Arturo = () => {
   const [task, setTask] = useState([]);
+  const noTaskMessage = "No hay tareas pendientes.";
 
   useEffect(() => {
+    if (localStorage.getItem("task") === null) {
+      localStorage.setItem("task", JSON.stringify([{ task: noTaskMessage }]));
+    }
     setTask(JSON.parse(localStorage.getItem("task")));
   }, []);
 
   useEffect(() => {
+    if (task[0] !== undefined) {
+      if (task[0].task === noTaskMessage && task[1] !== undefined) {
+        setTask(task.filter((item) => item.task !== noTaskMessage));
+      }
+    }
     localStorage.setItem("task", JSON.stringify(task));
   }, [task]);
 
@@ -20,6 +29,7 @@ const Arturo = () => {
 
   return (
     <section>
+      <h1>Arturito</h1>
       <AddContent state={GetState} />
       <ShowContent state={task} />
     </section>
--- src/components/ShowContent/index.js
+++ src/components/ShowContent/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const ShowContent = ({ state }) => {
-  const showList = state.map((e, index) => <li key={index}>{e.task}</li>);
+  const showList = state.map((item, index) => <li key={index}>{item.task}</li>);
   return (
     <section>
-      <h1>Arturito</h1>
+      <h3>Lista de tareas</h3>
       <ul>{showList}</ul>
     </section>
   );
